c2b17dcd909177238962d36dadb07e78ac352efc
vikrant-001 <121452566+vikrant-001@users.noreply.github.com>
2023-08-17 20:51:18 +0530

    button

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,1 +1,17 @@
-const 
+import classes from "./Header.module.css"
+import { Fragment } from "react"
+import mealImage from "../../assets/top-view-table-full-delicious-food-composition.jpg"
+import HeaderCartButton from "./HeaderCartButton"
+const Header = (props) => {
+    return (<Fragment>
+        <header className={classes.header}>
+            <h1>ReactMeals</h1>
+            <HeaderCartButton/>
+        </header>
+        <div className= {classes['main-image']}>
+            <img src={mealImage} alt= "A table full of delicious food"/>
+        </div>
+    </Fragment>)
+}
+
+export default Header;
